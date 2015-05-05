2d486d26477c6203d7661f9ce0619f67b3b987af
carlcafl <ccarmona@gmail.com>
2015-05-05 16:14:44 -0500

    Corrección de menú responsive

--- public/js/projects.js
+++
@@ -1,8 +0,0 @@
-angularModule.controller('ProjectListCtrl',['$scope', '$http',ProjectsController]);
-
-function ProjectsController($scope, $http) {
-	$http.get(backendURL + '/platforms').success(function(data) {
-		$scope.platforms = data;
-		alert('success!');
-	});
-};
