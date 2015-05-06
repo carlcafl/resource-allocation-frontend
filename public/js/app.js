8f22d9ae63fe47da441f421c56b3e359bd3321c6
carlcafl <ccarmona@gmail.com>
2015-05-06 15:18:35 -0500

    Adición de ordenamiento en grids

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 var backendURL = "https://sura-resource-alloc-backend.herokuapp.com/api/rest";
 
-var angularModule = angular.module('resourceAllocationApp', [ 'ngRoute' ])
+var angularModule = angular.module('resourceAllocationApp', [ 'ngRoute', 'ngTable' ])
 		.config(function($routeProvider) {
 			$routeProvider.
 			when('/', {
@@ -25,26 +25,56 @@ var angularModule = angular.module('resourceAllocationApp', [ 'ngRoute' ])
 			});
 		});
 
-angularModule.controller('PlatformListCtrl',['$scope', '$http',PlatformsController]);
-
-function PlatformsController($scope, $http) {
+angularModule.controller('PlatformListCtrl',function($scope, $http, $filter, ngTableParams) {
 	waitingDialog.show('Cargando...');
 	$http.get(backendURL + '/platforms'
 	).success(function(data) {			
 		$scope.platforms = data;
+		$scope.platformsTable = new ngTableParams({
+	        page: 1,            // show first page
+	        count: 10,          // count per page
+	        sorting: {
+	            shortName: 'asc'     // initial sorting
+	        }
+	    }, {
+	        total: data.length, // length of data
+	        getData: function($defer, params) {
+	            // use build-in angular filter
+	            var orderedData = params.sorting() ?
+	                                $filter('orderBy')(data, params.orderBy()) :
+	                                	data;
+
+	            $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+	        }
+	    });
 		waitingDialog.hide();
 	});
-};
+	
+});
 
-angularModule.controller('ProjectListCtrl',['$scope', '$http',ProjectsController]);
-
-function ProjectsController($scope, $http) {
+angularModule.controller('ProjectListCtrl',function ProjectsController($scope, $http, $filter, ngTableParams) {
 	waitingDialog.show('Cargando...');
 	$http.get(backendURL + '/projects'
 	).success(function(data) {			
 		$scope.projects = data;
+		$scope.projectsTable = new ngTableParams({
+	        page: 1,            // show first page
+	        count: 10,          // count per page
+	        sorting: {
+	            name: 'asc'     // initial sorting
+	        }
+	    }, {
+	        total: data.length, // length of data
+	        getData: function($defer, params) {
+	            // use build-in angular filter
+	            var orderedData = params.sorting() ?
+	                                $filter('orderBy')(data, params.orderBy()) :
+	                                	data;
+
+	            $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+	        }
+	    });
 		waitingDialog.hide();
 	});
-};
-
+});
 
